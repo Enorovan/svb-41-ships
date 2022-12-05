2605ce88ddda4c4585310a56b27ad5cc7f55e3e5
Enorovan <68027106+Enorovan@users.noreply.github.com>
2022-12-05 14:12:38 +0100

    defender done

--- defender.ts
+++ defender.ts
@@ -1,27 +1,39 @@
 import * as svb from '@svb-41/core'
 
-type Data = {}
+type Data = { x: number, y: number }
 
-export const data: Data = {}
-export const ai: svb.AI<Data> = ({ stats, radar, ship }) => {
-  const near = svb.radar.nearestEnemy(radar, stats.team, stats.position)
-  if (near) {
-    const source = stats.position
-    const target = near.enemy.position
-    const threshold = 4 / Math.sqrt(near.dist2)
-    const speed = stats.weapons[0]?.bullet.position.speed
-    const delay = Math.sqrt(near.dist2) / speed
-    const resAim = svb.geometry.aim({
-      ship,
-      source,
-      target,
-      threshold,
-      delay,
-      weapon: 0,
-    })
-    if (resAim.id === svb.Instruction.FIRE) return ship.fire()
-    return resAim
+export let hasTurned = false
+export const data: svb.ship.Position = {
+  pos: {
+    x: 1000,
+    y: 400,
+  },
+  direction: 0,
+  speed: 0,
+}
+export const ai: svb.AI<svb.ship.Position> = ({
+  stats,
+  radar,
+  ship,
+  comm,
+  memory,
+}) => {
+  if (radar.length > 0) {
+    const near = svb.radar.nearestEnemy(radar, stats.team, stats.position)
+    if (near) {
+      comm.sendMessage(near.enemy.position)
+      const source = stats.position
+      const target = near.enemy.position
+      return svb.geometry.aim({ ship, source, target, threshold: 0.01 })
+    }
+    return ship.thrust(0.1 - stats.position.speed)
   }
-
-  return ship.thrust(0.1 - stats.position.speed)
+  if (!hasTurned) {
+    const source = stats.position
+    const target = data
+    if (stats.position.direction === data.direction) hasTurned = true
+    return svb.geometry.aim({ ship, source, target, threshold: 0.01 })
+  } else {
+    return ship.thrust(0.1 - stats.position.speed)
+  }
 }
